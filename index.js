bf53f46eabc05b48ca25fd2b5831495ae49614a9
probably-not-porter <pelibby16@earlham.edu>
2020-11-21 22:40:53 -0500

    add lobby

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const server = app.listen(PORT, function (){
 app.use(express.static("public"));
 
 // Game Setup
-class CardType {
+class CardType { // set up all the information for a card
     constructor(name, image, color, type, cost, activation, quantity){
         this.name = name;
         this.image = image;
@@ -27,49 +27,109 @@ class CardType {
     }
 }
 
-// blue
-let wheat_field = new CardType("Wheat Field", "Wheat_Field.svg", "blue", "wheat", 1, [1], 6);
-let ranch = new CardType("Ranch", "Ranch.svg", "blue", "cow", 1, [2], 6);
-let forest = new CardType("Forest", "Forest.svg", "blue", "gear", 3, [5], 6);
-let mine = new CardType("Mine", "Mine.svg", "blue", "gear", 6, [9], 6);
-let apple_orchard = new CardType("Apple Orchard", "Apple_Orchard.svg", "green","wheat", 3, [10], 6);
+class Player { // create a new player
+    constructor(name, id){
+        this.name = name;
+        this.id = id;
+        this.cards = [
+            new CardType("Wheat Field", "Wheat_Field.svg", "blue", "wheat", 1, [1], 1),
+            new CardType("Bakery", "Bakery.svg", "green", "bread", 1, [2,3], 1)
+        ];
+        this.coins = 0;
+    }
+}
 
-// green
-let bakery = new CardType("Bakery", "Bakery.svg", "green", "bread", 1, [2,3], 6);
-let convenience_store = new CardType("Convenience Store", "Convenience_Store.svg", "green", "bread", 2,4, 6);
-let cheese_factory = new CardType("Cheese Factory", "Cheese_Factory.svg", "green", "factory", 5, 7, 6);
-let furniture_factory = new CardType("Furniture Factory", "Furniture_Factory.svg", "green", "factory", 3, 8, 6);
-let fruit_and_vegetable_market = new CardType("Fruit and Vegetable Market", "Fruit_and_Vegetable_Market.svg", "green", "fruit", 2, [11,12], 6);
-
-// red
-let cafe = new CardType("Cafe", "Cafe.svg", "red", "cup", 2, 3, 6);
-let family_restaurant = new CardType("Family Restaurant", "Family_Restaurant.svg", "red", "cup", 3, [9,10], 6)
-
-// purple
-let stadium = new CardType("Stadium", "Stadium.svg", "purple", "tower", 7, 6, 4);
-let tv_station = new CardType("TV Station", "TV_Station.svg", "purple", "tower", 7, 6, 4);
-let business_center = new CardType("Business Cetner", "Business_Center.svg", "purple", "tower", 8, 6, 4);
-
-// gold
-let train_station = new CardType("Train Station", "Train_Station", "gold", "tower", 4, null, null);
-let shopping_mall = new CardType("Shopping Mall", "Shopping_Mall.svg", "gold", "tower", 10, null, null);
-let amusement_park = new CardType("Amusement Park", "Amusement_Park.svg", "gold", "tower", 16, null, null);
-let radio_tower = new CardType("Radio Tower", "Radio_Tower.svg", "gold", "tower", 22, null, null);
-
-let game_stack = [wheat_field, ranch, forest, mine, apple_orchard, bakery, convenience_store, 
-    cheese_factory, furniture_factory, fruit_and_vegetable_market, cafe, family_restaurant, 
-    stadium, tv_station, business_center
-]
-
-//console.log(game_stack)
+class BoardState { // create a new board state
+    constructor(name, id){
+        this.id = id;
+        this.state = 0; // 0 for waiting, 1 for in play
+        this.name = name;
+        this.market = [
+            new CardType("Wheat Field", "Wheat_Field.svg", "blue", "wheat", 1, [1], 6), 
+            new CardType("Ranch", "Ranch.svg", "blue", "cow", 1, [2], 6), 
+            new CardType("Forest", "Forest.svg", "blue", "gear", 3, [5], 6), 
+            new CardType("Mine", "Mine.svg", "blue", "gear", 6, [9], 6), 
+            new CardType("Apple Orchard", "Apple_Orchard.svg", "green","wheat", 3, [10], 6), 
+            new CardType("Bakery", "Bakery.svg", "green", "bread", 1, [2,3], 6), 
+            new CardType("Convenience Store", "Convenience_Store.svg", "green", "bread", 2,4, 6), 
+            new CardType("Cheese Factory", "Cheese_Factory.svg", "green", "factory", 5, 7, 6), 
+            new CardType("Furniture Factory", "Furniture_Factory.svg", "green", "factory", 3, 8, 6), 
+            new CardType("Fruit and Vegetable Market", "Fruit_and_Vegetable_Market.svg", "green", "fruit", 2, [11,12], 6), 
+            new CardType("Cafe", "Cafe.svg", "red", "cup", 2, 3, 6), 
+            new CardType("Family Restaurant", "Family_Restaurant.svg", "red", "cup", 3, [9,10], 6), 
+            new CardType("Stadium", "Stadium.svg", "purple", "tower", 7, 6, 4), 
+            new CardType("TV Station", "TV_Station.svg", "purple", "tower", 7, 6, 4), 
+            new CardType("Business Cetner", "Business_Center.svg", "purple", "tower", 8, 6, 4), 
+            new CardType("Train Station", "Train_Station.svg", "gold", "tower", 4, null, null), 
+            new CardType("Shopping Mall", "Shopping_Mall.svg", "gold", "tower", 10, null, null), 
+            new CardType("Amusement Park", "Amusement_Park.svg", "gold", "tower", 16, null, null), 
+            new CardType("Radio Tower", "Radio_Tower.svg", "gold", "tower", 22, null, null)
+        ];
+        this.players = [];
+        this.turn = 0;
+        this.timer = 300;
+    }
+}
+var _game_list = [
+    
+];
 
 // Socket Setup
 const io = socket(server);
 
 io.on("connection", function (socket) {
     console.log("--> Made socket connection");
-    socket.on("request_boardstate", function() {
-        io.emit("display_boardstate", game_stack);
+
+    socket.on("join_game", function(data) {
+        socket.join(data[0])
+        let game = null;
+        let self_player = new Player("test player name", data[1]);
+        for (x in _game_list){
+            if (_game_list[x].id == data[0]){
+                game = _game_list[x]
+            }
+        }
+        if (game != null){
+            if (game.players.length < 4){
+                game.players.push(self_player);
+            }else{
+                console.log("GAME FULL!");
+            }
+            io.emit("receive_gamelist", _game_list);
+        }else{
+            console.log("GAME DOESNT EXIST!");
+        }
+    });
+    socket.on("start_game", function(id) {
+        for (x in _game_list){
+            if (_game_list[x].id == id){
+                _game_list[x].state = 1;
+            }
+        }
+        io.emit("receive_gamelist", _game_list);
+    });
+
+    socket.on("create_game", function(data) {
+        socket.join(data[1])
+        let new_game = new BoardState(data[0], data[1]);
+        let self_player = new Player("test player name", data[2]);
+        new_game.players.push(self_player);
+        _game_list.push(new_game);
+        io.emit("receive_gamelist", _game_list);
+    });
+    socket.on("request_gamelist", function() {
+        io.emit("receive_gamelist", _game_list);
+    });
+
+
+    socket.on("request_boardstate", function(id) { // client is requesting boardstate
+        let game = null;
+        for (x in _game_list){
+            if (_game_list[x].id == id){
+                game = _game_list[x]
+            }
+        }
+        io.to(id).emit("update_boardstate", game); // send boardstate to client
     });
     
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,162 @@
 $( document ).ready(function() {
     const socket = io();
-    socket.emit("request_boardstate");
+    let self_id = makeid(12);
+    let in_game = false;
 
-    socket.on("display_boardstate", function (data) {
-        console.log('displaying cards...');
+    function start_current(id){
+        console.log('game starting');
+        socket.emit("start_game", id);
+        socket.emit("request_boardstate", id);
+    }
 
-        for (let x = 0; x < data.length; x++){
-            console.log(data[x]);
-            let card = data[x];
+    socket.emit("request_gamelist"); // request boardstate
+
+    // create new game
+    document.getElementById("new_game").addEventListener("click", function () {
+        var name = prompt("Enter a name for your game");
+        if (name){
+            socket.emit("create_game", [name, makeid(10), self_id]);
+            document.getElementById("lobby").style.zIndex = 0;
+            document.getElementById("waiting").style.zIndex = 1;
+        }
+    }); 
+
+    socket.on("self_data", function (ip) {
+        console.log('Registered as player');
+        self_ip = ip;
+        console.log(self_ip);
+        
+    })
+
+    socket.on("receive_gamelist", function (list) {
+        if (in_game == false){
+            // update waiting screen
+            document.getElementById("waiting").innerText = "";
+            let current_game = null;
+            for (let x = 0; x < list.length; x++){
+                for (let y = 0; y < list[x].players.length; y++){
+                    if (list[x].players[y].id == self_id){
+                        current_game = list[x];
+                    }
+                }
+            }
+            if (current_game){
+                if (current_game.state == 1){ // GAME START, SWITCH SCREEN
+                    console.log('game is starting');
+                    in_game = true;
+                    document.getElementById("game").style.zIndex = 1;
+                    document.getElementById("waiting").style.zIndex = 0;
+                }
+                document.getElementById("waiting").innerText = current_game.name + " (waiting) \n" + current_game.players.length + "/4 \n\n";
+                for (x in current_game.players){
+                    document.getElementById("waiting").innerText += "\n" + current_game.players[x].name + "\n";
+                }
+                if (current_game.players.length > 1){
+                    document.getElementById("waiting").innerHTML += "<button id='button-"+current_game.id+"'>START GAME</button>";
+                    document.getElementById("button-" + current_game.id).addEventListener("click", function () {
+                        start_current(current_game.id)
+                    });
+                }
+            }
+
+            // update lobby board
+            var game_list = document.getElementById("active_games");
+            game_list.innerHTML = "";
+            for (let x = 0; x < list.length; x++){
+                let game  = list[x];
+                if (game.state == 0){
+                    var li = document.createElement("li");
+                    li.className = game.id;
+                    li.innerText = game.name + " (" + game.players.length + "/4 players)";
+                    game_list.appendChild(li);
+
+
+                    // JOIN GAME
+                    li.addEventListener("click", function () {
+                        console.log(li.className)
+                        socket.emit("join_game", [li.className, self_id]);
+                        document.getElementById("lobby").style.zIndex = 0;
+                        document.getElementById("waiting").style.zIndex = 1;
+                    });
+                }
+            }
+        }else{
+
+        }
+    });
+
+    socket.on("update_boardstate", function (state) { // display new boardstate
+        // MARKET
+        console.info(state);
+        console.log('--> display market');
+        let market_cards = state.market;
+        document.getElementById("decks").innerHTML = "";
+        for (let x = 0; x < market_cards.length; x++){
+            let card = market_cards[x];
+            var card_elem = document.createElement("div");
+            var card_img = document.createElement("img");
+            var card_num = document.createElement("span");
+            card_elem.className = "card";
+            card_img.src = "../img/" + card.image; 
+            card_num.innerText = "x" +  card.quantity;
+            card_elem.appendChild(card_img);
+            if (card.quantity != null){ card_elem.appendChild(card_num); }
+            document.getElementById("decks").appendChild(card_elem)
+        }
+
+        // SELF
+        console.log('--> display player cards');
+        let self_player = null;
+        let stats = {
+            "coins": 0,
+            "bread": 0,
+            "cup": 0,
+            "fruit": 0,
+            "tower": 0,
+            "cow": 0,
+            "factory": 0,
+            "gear": 0,
+            "wheat": 0
+        }
+        for (x in state.players){
+            if (state.players[x].id = self_id){
+                self_player = state.players[x]
+            }
+        }
+        for (let x = 0; x < self_player.cards.length; x++){
+            let card = self_player.cards[x];
+            // stats
+            stats[card.type] += 1;
 
             var card_elem = document.createElement("div");
             var card_img = document.createElement("img");
             var card_num = document.createElement("span");
-
             card_elem.className = "card";
             card_img.src = "../img/" + card.image; 
-
             card_num.innerText = "x" +  card.quantity;
-
             card_elem.appendChild(card_img);
-            card_elem.appendChild(card_num);
-
-            document.getElementById("decks").appendChild(card_elem)
+            if (card.quantity != null){ card_elem.appendChild(card_num); }
+            document.getElementById("field").appendChild(card_elem);
         }
+        document.getElementById("coins-val").innerText = stats["coins"];
+        document.getElementById("bread-val").innerText = stats["bread"];
+        document.getElementById("cup-val").innerText = stats["cup"];
+        document.getElementById("fruit-val").innerText = stats["fruit"];
+        document.getElementById("tower-val").innerText = stats["tower"];
+        document.getElementById("cow-val").innerText = stats["cow"];
+        document.getElementById("factory-val").innerText = stats["factory"];
+        document.getElementById("gear-val").innerText = stats["gear"];
+        document.getElementById("wheat-val").innerText = stats["wheat"];
     });
+    
+    function makeid(n) {
+        var result           = '';
+        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for ( var i = 0; i < n; i++ ) {
+          result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    }
 });
+
